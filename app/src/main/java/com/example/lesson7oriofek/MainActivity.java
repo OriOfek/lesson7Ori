ae316413f2034393490c3934bbdf71fdc0eb3c52
liemb <lb6031@bs.amalnet.k12.il>
2020-05-26 09:49:12 +0300

    work xml

--- app/src/main/java/com/example/lesson7oriofek/MainActivity.java
+++ app/src/main/java/com/example/lesson7oriofek/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.lesson7oriofek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout screen;
+    Switch s;
+    RadioGroup rg;
+    RadioButton rb1,rb2,rb3,rb4;
+    boolean isPressed;
+    Boolean mode;
+    Color []colores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rg = (RadioGroup) findViewById(R.id.rg);
+        rb1 = (RadioButton) findViewById(R.id.rb1);
+        rb2 = (RadioButton) findViewById(R.id.rb2);
+        rb3 = (RadioButton) findViewById(R.id.rb3);
+        rb4 = (RadioButton) findViewById(R.id.rb4);
+        s = (Switch) findViewById(R.id.switch1);
+        screen = (LinearLayout) findViewById(R.id.screen);
+        //colores = {Color.GREEN, Color.BLUE, Color.RED, Color.YELLOW};
+    }
+
+    public void changeMode(View view) {
+        mode = !mode;
+    }
+
+    public void isCliced(View view) {
+        isPressed = true;
+        if (mode)
+        {
+
+        }
     }
 }
